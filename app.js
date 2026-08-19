75d6a5d553d5a82051caaa596a8c7f7460d05c8d
“24104044-jyotika” <24104044@apsit.edu.in>
2026-08-19 14:38:42 +0530

    Add Docker files

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-	res.send('DevOps CI/CD Project Working');
+	res.send('How was the session:');
+        res.send('The session was good');
 	});
 app.listen(3000, () => {
 	console.log('Running on port 3000');
 	});
+
