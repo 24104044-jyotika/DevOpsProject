77b7d0f87d6b4fb49ddd37e052c78ed13ba3f2b4
Jyotika Rao <24104044@apsit.edu.in>
2026-08-19 14:41:04 +0530

    Fix response message formatting in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
 	res.send('How was the session:');
-        res.send('The session was good');
+    res.send('The session was good');
 	});
 app.listen(3000, () => {
 	console.log('Running on port 3000');
